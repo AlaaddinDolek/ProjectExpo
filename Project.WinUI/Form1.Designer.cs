b59c80a2a14cf7503da9f20e8f9b3a29f3d6816e
Alaaddin Dolek <alaaddin.dolek@gmail.com>
2023-03-02 13:44:08 +0300

    Tasarım taslağı hazırlandı

--- Project.WinUI/Form1.Designer.cs
+++ Project.WinUI/Form1.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnDecline = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblSaloon = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblActivity = new System.Windows.Forms.Label();
+            this.lstAvailableStands = new System.Windows.Forms.ListBox();
+            this.lstIssue = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.lblIssue = new System.Windows.Forms.Label();
+            this.btnIssue = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Enabled = false;
+            this.btnAccept.Location = new System.Drawing.Point(278, 677);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(85, 32);
+            this.btnAccept.TabIndex = 0;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            // 
+            // btnDecline
+            // 
+            this.btnDecline.Enabled = false;
+            this.btnDecline.Location = new System.Drawing.Point(397, 677);
+            this.btnDecline.Name = "btnDecline";
+            this.btnDecline.Size = new System.Drawing.Size(84, 32);
+            this.btnDecline.TabIndex = 0;
+            this.btnDecline.Text = "Decline";
+            this.btnDecline.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(54, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(122, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // lblSaloon
+            // 
+            this.lblSaloon.Location = new System.Drawing.Point(54, 70);
+            this.lblSaloon.Name = "lblSaloon";
+            this.lblSaloon.Size = new System.Drawing.Size(122, 29);
+            this.lblSaloon.TabIndex = 2;
+            this.lblSaloon.Text = "Select Saloon";
+            this.lblSaloon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(359, 102);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(122, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // lblActivity
+            // 
+            this.lblActivity.Location = new System.Drawing.Point(359, 70);
+            this.lblActivity.Name = "lblActivity";
+            this.lblActivity.Size = new System.Drawing.Size(122, 29);
+            this.lblActivity.TabIndex = 2;
+            this.lblActivity.Text = "Select Activity";
+            this.lblActivity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstAvailableStands
+            // 
+            this.lstAvailableStands.FormattingEnabled = true;
+            this.lstAvailableStands.ItemHeight = 16;
+            this.lstAvailableStands.Location = new System.Drawing.Point(54, 153);
+            this.lstAvailableStands.Name = "lstAvailableStands";
+            this.lstAvailableStands.Size = new System.Drawing.Size(427, 420);
+            this.lstAvailableStands.TabIndex = 3;
+            // 
+            // lstIssue
+            // 
+            this.lstIssue.FormattingEnabled = true;
+            this.lstIssue.ItemHeight = 16;
+            this.lstIssue.Location = new System.Drawing.Point(568, 153);
+            this.lstIssue.Name = "lstIssue";
+            this.lstIssue.Size = new System.Drawing.Size(366, 420);
+            this.lstIssue.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(707, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Enabled = false;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(568, 78);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(122, 24);
+            this.comboBox4.TabIndex = 1;
+            // 
+            // lblIssue
+            // 
+            this.lblIssue.Location = new System.Drawing.Point(565, 46);
+            this.lblIssue.Name = "lblIssue";
+            this.lblIssue.Size = new System.Drawing.Size(122, 29);
+            this.lblIssue.TabIndex = 2;
+            this.lblIssue.Text = "Select Issue";
+            this.lblIssue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnIssue
+            // 
+            this.btnIssue.Enabled = false;
+            this.btnIssue.Location = new System.Drawing.Point(707, 108);
+            this.btnIssue.Name = "btnIssue";
+            this.btnIssue.Size = new System.Drawing.Size(227, 28);
+            this.btnIssue.TabIndex = 0;
+            this.btnIssue.Text = "Add Issue";
+            this.btnIssue.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(704, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Write Details";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCheck.Location = new System.Drawing.Point(54, 596);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(427, 68);
+            this.lblCheck.TabIndex = 2;
+            this.lblCheck.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1062, 737);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstIssue);
+            this.Controls.Add(this.lstAvailableStands);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.lblIssue);
+            this.Controls.Add(this.lblActivity);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.lblSaloon);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnDecline);
+            this.Controls.Add(this.btnIssue);
+            this.Controls.Add(this.btnAccept);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnDecline;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblSaloon;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblActivity;
+        private System.Windows.Forms.ListBox lstAvailableStands;
+        private System.Windows.Forms.ListBox lstIssue;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label lblIssue;
+        private System.Windows.Forms.Button btnIssue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCheck;
     }
 }
 
