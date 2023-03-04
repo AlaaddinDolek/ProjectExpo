285e71b3e813c958e67206a9838552d535456410
Barkın Atıcı <brknatici@hotmail.com>
2023-03-04 13:45:27 +0300

    Design değiti

--- Project.WinUI/Form2.Designer.cs
+++ Project.WinUI/Form2.Designer.cs
@@ -414,6 +414,7 @@
             this.Controls.Add(this.btnAccept);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
