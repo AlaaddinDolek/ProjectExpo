84a023f1e3718b59391ae9f4b7563a532799b0d1
Alaaddin Dolek <alaaddin.dolek@gmail.com>
2023-03-04 15:17:59 +0300

    Order Repository' ye update metodu eklendi

--- Project.BLL/GenericRepository/ConcRep/OrderRepository.cs
+++ Project.BLL/GenericRepository/ConcRep/OrderRepository.cs
@@ -10,18 +10,24 @@ using System.Threading.Tasks;
 
 namespace Project.BLL.GenericRepository.ConcRep
 {
-    public class OrderRepository:BaseRepository<Order>
+    public class OrderRepository : BaseRepository<Order>
     {
         MyContext _orderDb;
         public OrderRepository()
         {
             _orderDb = DBTool.DbInstance;
         }
-
-        public void UpdateDate()
+        public void UpdateDate(string saloonName, int standNumber, int delayNumber)
         {
+            IQueryable y = _orderDb.Orders.Where(x => x.Stand.Saloon.SaloonName == $"{saloonName}" && x.Stand.StandNo == $"{standNumber}");
 
+            foreach (Order item in y)
+            {
+                item.DueDate.AddDays(delayNumber);
+                _orderDb.SaveChanges();
+            }
         }
-
     }
+
 }
+
